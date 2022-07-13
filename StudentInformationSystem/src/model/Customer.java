baea75a6b6f273b8e0aebe6207cf1c5fcc7112d7
Siddhesh512 <62982716+Siddhesh512@users.noreply.github.com>
2022-07-13 13:44:32 +0530

    Update Customer.java

--- StudentInformationSystem/src/model/Customer.java
+++ StudentInformationSystem/src/model/Customer.java
@@ -1,5 +1,20 @@
 package model;
 
 public class Customer {
+  int customerId;
+	String customerName;
+	int Age;
+	public int getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
 
 }
